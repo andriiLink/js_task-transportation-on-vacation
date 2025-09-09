3dbea55c1c2d0826f9a8a1377459a8c8b12502f5
Andrii Linkevych <andrii.linkevych.21@pnu.edu.ua>
2025-09-09 22:41:18 +0300

    Solution after review

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,18 +4,24 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let rentCost = 0;
+  const DAILY_COST = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
 
-  for (let i = 0; i < days; i++) {
-    rentCost += 40;
+  let rentCost = DAILY_COST * days;
+
+  if (days >= LONG_TERM) {
+    rentCost -= LONG_TERM_DISCOUNT;
+
+    return rentCost;
   }
 
-  if (days >= 7) {
-    rentCost -= 50;
-  }
+  if (days >= SHORT_TERM) {
+    rentCost -= SHORT_TERM_DISCOUNT;
 
-  if (days >= 3 && days < 7) {
-    rentCost -= 20;
+    return rentCost;
   }
 
   return rentCost;
