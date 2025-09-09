7fb023910152b6a31cfb98632fb82eef83fca9e5
Andrii Linkevych <andrii.linkevych.21@pnu.edu.ua>
2025-09-09 21:46:15 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let rentCost = 0;
+
+  for (let i = 0; i < days; i++) {
+    rentCost += 40;
+  }
+
+  if (days >= 7) {
+    rentCost -= 50;
+  }
+
+  if (days >= 3 && days < 7) {
+    rentCost -= 20;
+  }
+
+  return rentCost;
 }
 
 module.exports = calculateRentalCost;
